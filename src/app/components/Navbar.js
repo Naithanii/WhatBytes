0b9691042dcbd15b220e078151a9c0cef3535431
Naithanii <Naithani04@outlook.com>
2025-01-24 14:08:20 +0530

    l

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Link from "next/link";
+// import Link from "next/link";
 import Image from "next/image";
 import logo from "../assets/wlogo.jpg";
 import useravatar from "../assets/user-avatar.jpg";
